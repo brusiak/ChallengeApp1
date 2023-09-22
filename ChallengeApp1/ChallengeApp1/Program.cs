d86c86f1ac569c59b0b7ac0ff0d96580c8906832
brusiak <brusiak90@gmail.com>
2023-09-22 16:42:36 +0200

    zadanie dzien 4

--- ChallengeApp1/ChallengeApp1/Program.cs
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// praca domowa
+
+var name = "Ewa";
+char sex = 'K';
+var age = 33;
+
+if (name == "Ewa" && age == 25 && sex == 'K')
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && sex == 'K' && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (name == "Ewa" && age <= 17  && sex == 'k')
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
