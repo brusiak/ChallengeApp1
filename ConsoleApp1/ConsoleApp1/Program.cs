7593101cac499d6b35eb583ac136c773b3ebfb5f
brusiak <brusiak90@gmail.com>
2023-11-30 12:35:38 +0100

    pierwszy program

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var name = "Ewa";
+var sex = "kobieta";
+int wiek = 33;
+
+if (sex == "kobieta" && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (wiek < 18)
+{
+    Console.WriteLine("Niepełnoletni.");
+}
+else
+{
+    Console.WriteLine("Inne.");
+}
